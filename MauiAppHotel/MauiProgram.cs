6481be4ceb5932132afd9c075c839d3bdbd8cfd0
Douglas Francisco <douglas04_298@hotmail.com>
2025-11-01 10:24:54 -0300

    criando pagina Sobre

--- MauiAppHotel/MauiProgram.cs
+++ MauiAppHotel/MauiProgram.cs
@@ -13,6 +13,8 @@ namespace MauiAppHotel
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("ComicRelief-Bold.ttf", "ComicReliefBold");
+                    fonts.AddFont("ComicRelief-Regular.ttf", "ComicRelief");
                 });
 
 #if DEBUG
